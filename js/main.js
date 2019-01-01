60aec459dd5c44b1cc596677af1e5056e743b604
Ante Petrovic <lelomacbook@gmail.com>
2019-01-01 17:45:45 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $(".hamburgerDiv").click(function() {
+        $("nav ul").fadeToggle(400);
+        //$("nav ul")
+    });
+
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
